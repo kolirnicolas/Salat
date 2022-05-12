130b16379b0c9b39fcb2bc14022ecc597481f29d
nicolas <kolirzet09@gmail.com>
2022-05-12 18:39:08 +0300

    первое задание
    Signed-off-by: nicolas <kolirzet09@gmail.com>

--- src/lesson2/Exercise1.java
+++ src/lesson2/Exercise1.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 
 public class Exercise1 {
     public static void main(String[] args) {
+
         Stream.of("Amazon", "Bew", "Cce", "Dde", "IeZ", "Fweew", "heZ")
                 .filter(s -> s.startsWith("A") || s.endsWith("Z"))
                 .collect(Collectors.toSet())
