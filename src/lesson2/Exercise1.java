9235ae3c32f78d7a336bf80ef5df2b8bfc02e118
nicolas <kolirzet09@gmail.com>
2022-05-12 21:15:28 +0300

    keun

--- src/lesson2/Exercise1.java
+++ src/lesson2/Exercise1.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class Exercise1 {
     public static void main(String[] args) {
 
-        Stream.of("Amazon", "Bew", "Cce", "Dde", "IeZ", "Fweew", "heZ")
+        Stream.of("Amazon", "Cce", "Dde", "IeZ", "Fweew", "heZ")
                 .filter(s -> s.startsWith("A") || s.endsWith("Z"))
                 .forEach(System.out::println);
     }
