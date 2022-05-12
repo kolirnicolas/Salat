6f3caf61beb20dba2f69847ca0c7ef1847f8670c
nicolas <kolirzet09@gmail.com>
2022-05-12 21:14:44 +0300

    keun

--- src/lesson2/Exercise1.java
+++ src/lesson2/Exercise1.java
@@ -1,13 +1,8 @@
 package lesson2;
 
-import java.util.stream.Stream;
-
 public class Exercise1 {
     public static void main(String[] args) {
 
-        Stream.of("Amazon", "Bew", "Cce", "Dde", "IeZ", "Fweew", "heZ")
-                .filter(s -> s.startsWith("A") || s.endsWith("Z"))
-                .forEach(System.out::println);
     }
 }
 
